50a7a930b355bf3f86d05ec8abf756f4492712ee
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-12-30 12:45:18 -0600

    fixed name

--- frontend/src/libs/constants/seo.constants.ts
+++ frontend/src/libs/constants/seo.constants.ts
@@ -1,12 +1,12 @@
 export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
 
-export const SITE_NAME = 'TeaStream'
+export const SITE_NAME = 'Streamify'
 
 export const SITE_DESCRIPTION =
-	'TeaStream — это платформа для прямых трансляций, которая соединяет стримеров и зрителей. Делитесь своими увлечениями и присоединяйтесь к сообществу TeaStream!'
+	'Streamify — это платформа для прямых трансляций, которая соединяет стримеров и зрителей. Делитесь своими увлечениями и присоединяйтесь к сообществу TeaStream!'
 
 export const SITE_KEYWORDS = [
-	'TeaStream',
+	'Streamify',
 	'платформа для прямых трансляций',
 	'стриминг',
 	'прямые трансляции',

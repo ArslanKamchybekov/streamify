875ed1ce3cc251c0dc4d83bb2f39c44148747879
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-04-22 12:59:30 -0500

    GraphQL configuration

--- server/src/modules/auth/account/account.service.ts
+++ server/src/modules/auth/account/account.service.ts
@@ -4,12 +4,9 @@ import {
 	UnauthorizedException
 } from '@nestjs/common'
 import { hash, verify } from 'argon2'
-
 import type { User } from '@/prisma/generated'
 import { PrismaService } from '@/src/core/prisma/prisma.service'
-
 import { VerificationService } from '../verification/verification.service'
-
 import { ChangeEmailInput } from './inputs/change-email.input'
 import { ChangePasswordInput } from './inputs/change-password.input'
 import { CreateUserInput } from './inputs/create-user.input'
